40ec835267cfbd74d32b2ccec04dc2b8b72ea28b
Davit <davitmegrelishvili123@gmail.com>
2026-02-26 13:52:56 +0400

    Add input fields

--- src/user-recipe.ts
+++ src/user-recipe.ts
@@ -7,6 +7,27 @@
     let lowerDiv = document.createElement("div");
     const unifiedDiv = document.createElement("div");
 
+    let nameInput = document.createElement("input");
+    nameInput.id = 'name-input';
+    nameInput.placeholder = 'Name: '
+
+    let originInput = document.createElement("input");
+    originInput.id = 'origin-input';
+    originInput.placeholder = 'Origin: '
+
+    let ingredientsInput = document.createElement("textarea");
+    ingredientsInput.id = 'ingredients-input';
+    ingredientsInput.placeholder = 'Ingredients: '
+
+    let ytLinkInput = document.createElement("input");
+    ytLinkInput.id = 'video-input';
+    ytLinkInput.placeholder = 'Youtube Link:';
+
+
+    let instructionsInput = document.createElement("textarea");
+    instructionsInput.id = 'instructions-input';
+    instructionsInput.placeholder = 'Instructions: ';
+
     let inputDiv = document.createElement("div");
     inputDiv.className = 'input-div';
 
@@ -22,17 +43,22 @@
     inputDiv.appendChild(input);
 
 
-   addImage(input, inputDiv);
+    addImage(input, inputDiv);
 
 
     picDiv.className = 'pic-div';
     picDiv.appendChild(inputDiv);
 
     rightDiv.className = 'right-div';
+    rightDiv.appendChild(nameInput);
+    rightDiv.appendChild(originInput);
+    rightDiv.appendChild(ingredientsInput);
+    rightDiv.appendChild(ytLinkInput);
 
     upperDiv.className = 'upper-div';
 
     lowerDiv.className = 'lower-div';
+    lowerDiv.appendChild(instructionsInput);
 
     unifiedDiv.className = 'unified-div';
 
