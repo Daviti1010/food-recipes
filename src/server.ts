2de6da6d34de98f474975c229a569b149ba0568b
Davit <davitmegrelishvili123@gmail.com>
2026-04-22 16:44:08 +0400

    fix tsconfig and cloudinary

--- src/server.ts
+++ src/server.ts
@@ -42,9 +42,9 @@ declare module "express-session" {
 
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
 const storage = new CloudinaryStorage({
